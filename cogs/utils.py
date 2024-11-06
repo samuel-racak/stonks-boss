1c170cebe8daff9d940144eb54da44dbfdd13480
samuel-racak <samuel.racak@gmail.com>
2024-11-06 23:36:55 +0100

    feat: add exchange suffixes and utility function for full ticker retrieval

--- cogs/utils.py
+++ cogs/utils.py
@@ -24,3 +24,21 @@ def get_country_flag(country_name):
         return "".join([chr(ord(letter) + 127397) for letter in country.alpha_2])
 
     return ""
+
+
+exchange_suffixes = {
+    "NYSE": "",  # No suffix needed for NYSE
+    "NASDAQ": "",  # No suffix needed for NASDAQ
+    "Euronext Paris": ".PA",  # Euronext Paris (for example: ORA.PA for Orange)
+    "Börse Frankfurt": ".F",  # Frankfurt (example: DAI.F for Daimler)
+    "London Stock Exchange": ".L",  # LSE (example: VOD.L for Vodafone)
+    "BSE": ".BO",  # Bombay Stock Exchange (example: TATAMOTORS.BO for Tata Motors)
+    "Tokyo Stock Exchange": ".T",  # Tokyo (example: 7203.T for Toyota)
+    "Toronto Stock Exchange": ".TO",  # Toronto (example: RY.TO for Royal Bank of Canada)
+    "Hong Kong Stock Exchange": ".HK",  # Hong Kong (example: 0005.HK for HSBC)
+}
+
+
+def get_full_ticker(ticker, exchange):
+    """Get the full ticker symbol including the exchange suffix."""
+    return f"{ticker}{exchange_suffixes.get(exchange, '')}"  # default to NYSE/ NASDAQ
