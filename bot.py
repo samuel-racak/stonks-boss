628f93b2eb2f6cf5cafff7203c3a508ccdb82145
samuel-racak <samuel.racak@gmail.com>
2024-11-06 10:59:54 +0100

    feat: enhance bot functionality with financial data and message on member join

--- bot.py
+++ bot.py
@@ -1,21 +1,31 @@
 from discord.ext import commands
 from discord import Intents
-
 from config import BotConfig
 
-# from cogs import my_commands
-
+configuration = BotConfig()
 
 intents = Intents.default()
 intents.members = True
 
-
-bot = commands.Bot(command_prefix="!", intents=intents)
+# create bot instance
+bot = commands.Bot(command_prefix=configuration.COMMAND_PREFIX, intents=intents)
 
 
 @bot.event
 async def on_ready():
+    await bot.load_extension("cogs.finance_cog")
+    await bot.tree.sync()
     print(f"{bot.user.name} has connected to Discord!")
 
 
-bot.run(BotConfig().DISCORD_TOKEN)
+# welcome message
+@bot.event
+async def on_member_join(member):
+    channel = member.guild.system_channel
+    if channel:
+        await channel.send(
+            f"Welcome {member.mention}! I am {bot.user.name}. I can help you with financial data. Use the slash commands to interact with me."
+        )
+
+
+bot.run(configuration.DISCORD_TOKEN)
