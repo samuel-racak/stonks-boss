2ef52bc05c8a32cef1b49875501d6f85a677f9f0
samuel-racak <samuel.racak@gmail.com>
2024-11-06 23:00:08 +0100

    feat: add constants for embed colors and bot configuration

---
+++ constants.py
@@ -0,0 +1,9 @@
+# Embed colors
+EMBED_COLOR_PRIMARY = 0x4DE8BC  # Hexadecimal for a blue color
+EMBED_COLOR_SUCCESS = 0x2ECC71  # Green
+EMBED_COLOR_WARNING = 0xE67E22  # Orange
+EMBED_COLOR_ERROR = 0xE74C3C  # Red
+
+# Other potential constants
+BOT_NAME = "StonksBoss"
+FOOTER_TEXT = "Provided by Yahoo Finance API"
